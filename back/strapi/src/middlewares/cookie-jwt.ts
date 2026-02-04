929982a161f18343f491cbd83fc12899cf180232
Marko <markonikolicviser@gmail.com>
2026-02-04 16:17:51 +0100

    hotfix: cookie is not valid

--- back/strapi/src/middlewares/cookie-jwt.ts
+++ back/strapi/src/middlewares/cookie-jwt.ts
@@ -1,10 +1,30 @@
 export default (config, { strapi }) => {
-    return async (ctx, next) => {
-        const token = ctx.cookies.get('token', { signed: true });
-        if (token && !ctx.request.headers.authorization) {
-            ctx.request.headers.authorization = `Bearer ${token}`;
-        }
+  return async (ctx, next) => {
+    const cookieName = process.env.COOKIE_NAME || "token";
 
-        await next();
-    };
+    // If you use signed cookies, keep this:
+    // const token = ctx.cookies.get(cookieName, { signed: true });
+
+    // If you DON'T need signed cookies (JWT already signed), prefer this:
+    const token = ctx.cookies.get(cookieName);
+
+    const hadAuth = !!ctx.request.headers.authorization;
+
+    if (token && !hadAuth) {
+      ctx.request.headers.authorization = `Bearer ${token}`;
+    }
+
+    // ✅ Debug (safe): don't print full token
+    const tokenPreview = token
+      ? `${token.slice(0, 12)}...${token.slice(-8)}`
+      : null;
+
+    strapi.log.info(
+      `[cookie-jwt] ${ctx.method} ${ctx.url} ` +
+        `cookie:${!!token} authBefore:${hadAuth} authAfter:${!!ctx.request.headers.authorization} ` +
+        `tokenPreview:${tokenPreview}`
+    );
+
+    await next();
+  };
 };
--- front/helpers/constants.ts
+++ front/helpers/constants.ts
@@ -1,5 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337/api';
 export const TOKEN_KEY = 'token';
-export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''
-export const STRAPI_REDIRECT_URI = process.env.NEXT_PUBLIC_STRAPI_REDIRECT_URI || '#http://localhost:1337/api/auth/google/code'
-export const GOOGLE_URI = 'https://accounts.google.com/o/oauth2/v2/auth'
+export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+export const STRAPI_REDIRECT_URI =
+  process.env.NEXT_PUBLIC_STRAPI_REDIRECT_URI || 'http://localhost:1337/api/auth/google/code';
+export const GOOGLE_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
